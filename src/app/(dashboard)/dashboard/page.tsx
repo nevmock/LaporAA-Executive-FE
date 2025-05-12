de46028b80d6a6465366f8499d9fb0d94214a0a8
optimus9021 <ismaianugrah@gmail.com>
2025-05-12 17:36:51 +0700

    push

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -7,7 +7,8 @@ import EfisiensiCard from "./components/EfisiensiCard";
 import EffectivenessCard from "./components/EffectivenessCard";
 import DistribusiCard from "./components/DistribusiCard";
 import KepuasanCard from "./components/KepuasanCard";
-import EffectivenessChart from "@/app/(dashboard)/dashboard/components/EffectivenessChart";
+import SpedoChart from "@/app/(dashboard)/dashboard/components/SpedoChart";
+import LineChart from "@/app/(dashboard)/dashboard/components/LineChart";
 
 
 const API_URL = process.env.NEXT_PUBLIC_BE_BASE_URL;
@@ -34,19 +35,23 @@ export default function Home() {
 
     return (
         <div className="w-full h-screen bg-white p-6 overflow-hidden flex flex-col">
-    
+            <div className="flex flex-row flex-wrap gap-4 justify-between items-start mb-4">
+                <div className="flex-1 min-w-[300px]">
+                    <SpedoChart />
+                </div>
+                <div className="flex-1 min-w-[300px]">
+                    <LineChart />
+                </div>
+            </div>
+
+            
             {/* Statistik Cards */}
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mb-4">
+            {/* <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mb-4">
                 <EfisiensiCard />
                 <EffectivenessCard />
                 <DistribusiCard />
                 <KepuasanCard />
-            </div>
-
-            {/* Effectiveness Chart Container */}
-            <div className="flex-1 bg-gray-200 rounded-xl shadow overflow-hidden">
-                <EffectivenessChart />
-            </div>
+            </div> */}
 
             {/* Maps Container */}
             <div className="flex-1 bg-gray-200 rounded-xl shadow overflow-hidden">
